33f16eae0874c42ae9a4fba5a4396a768091ea82
Jefferaon <20103827@mail.wit.ie>
2023-03-02 17:07:36 +0000

    Changed MainActivity.kt to CoffeeActivity.kt

--- app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
@@ -3,7 +3,7 @@ package ie.setu.coffeeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CoffeeAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
