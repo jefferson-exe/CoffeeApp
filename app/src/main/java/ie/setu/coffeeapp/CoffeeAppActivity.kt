601953cc22f485a4923b8c75c750e71375cb8a9e
Jefferaon <20103827@mail.wit.ie>
2023-03-02 17:29:03 +0000

    Replaced timber dependency with kotlin timber extension

--- app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
@@ -2,10 +2,16 @@ package ie.setu.coffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
+
 
 class CoffeeAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffeeapp)
+
+        Timber.plant(Timber.DebugTree())
+
+        Timber.i("CoffeeApp Activity started..")
     }
 }
