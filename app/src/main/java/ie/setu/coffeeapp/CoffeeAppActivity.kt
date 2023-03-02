9b5d44785f443336831e7d679a73b461821172b9
Jefferaon <20103827@mail.wit.ie>
2023-03-02 20:16:35 +0000

    Added alternative version of add button

--- app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
@@ -2,6 +2,7 @@ package ie.setu.coffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import ie.setu.coffeeapp.databinding.ActivityCoffeeappBinding
 import timber.log.Timber
 import timber.log.Timber.i
@@ -21,7 +22,15 @@ class CoffeeAppActivity : AppCompatActivity() {
         i("CoffeeApp Activity started..")
 
         binding.btnAdd.setOnClickListener() {
-            i("add Button Pressed")
+            val placemarkTitle = binding.placemarkTitle.text.toString()
+            if (placemarkTitle.isNotEmpty()) {
+                i("add Button Pressed: $placemarkTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
         }
     }
 }
