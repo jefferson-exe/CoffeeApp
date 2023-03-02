b65a33d170856ab32cf468b1ceb5e5326ce5ba77
Jefferaon <20103827@mail.wit.ie>
2023-03-02 19:56:19 +0000

    Added event handling ActivityCoffeeappBinding outside the onCreate() function

--- app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/CoffeeAppActivity.kt
@@ -2,11 +2,13 @@ package ie.setu.coffeeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.coffeeapp.databinding.ActivityCoffeeappBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 
 class CoffeeAppActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCoffeeappBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffeeapp)
