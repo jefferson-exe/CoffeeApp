8d0806457304798af3c855b9aead867c1e18059b
Jefferaon <20103827@mail.wit.ie>
2023-03-03 13:58:51 +0000

    Created a new layout called card_coffeeapp.xml

--- app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppListActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppListActivity.kt
@@ -1,11 +1,17 @@
-package ie.setu.coffeeapp.main
+package ie.setu.coffeeapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.coffeeapp.R
+import ie.setu.coffeeapp.main.MainApp
 
 class CoffeeAppListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coffee_app_list)
+        app = application as MainApp
     }
 }
