8fb3f84ad9423ab07130fd26d95af12c78b415de
Jefferaon <20103827@mail.wit.ie>
2023-03-03 13:21:00 +0000

    Added new empty activity called CoffeeAppListActivity

---
+++ app/src/main/java/ie/setu/coffeeapp/main/CoffeeAppListActivity.kt
@@ -0,0 +1,11 @@
+package ie.setu.coffeeapp.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeAppListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee_app_list)
+    }
+}
