2c6304a9cffa44ce8ba667e79cc46d7d15cfb1d7
Jefferaon <20103827@mail.wit.ie>
2023-03-03 13:13:08 +0000

    Added Null Safety

--- app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppActivity.kt
@@ -4,42 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.setu.coffeeapp.databinding.ActivityCoffeeappBinding
+import ie.setu.coffeeapp.main.MainApp
 import ie.setu.coffeeapp.models.CoffeeAppModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class CoffeeAppActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityCoffeeappBinding
-
     var coffeeapp = CoffeeAppModel()
-    val coffees = ArrayList<CoffeeAppModel>()
+    lateinit var app : MainApp
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+            super.onCreate(savedInstanceState)
+            binding = ActivityCoffeeappBinding.inflate(layoutInflater)
+            setContentView(binding.root)
 
-        binding = ActivityCoffeeappBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        Timber.plant(Timber.DebugTree())
-
-        i("Placemark Activity started...")
-
-        binding.btnAdd.setOnClickListener() {
-            coffeeapp.title = binding.coffeeappTitle.text.toString()
-            coffeeapp.description = binding.description.text.toString()
-            if (coffeeapp.title.isNotEmpty()) {
-                coffees.add(coffeeapp.copy())
-
-                i("add Button Pressed: $coffeeapp.title")
-                for (i in coffees.indices)
-                { i("Placemark[$i]:${this.coffees[i]}") }
+            app = application as MainApp
+            i("CoffeeApp Activity started...")
+            binding.btnAdd.setOnClickListener() {
+                coffeeapp.title = binding.coffeeappTitle.text.toString()
+                coffeeapp.description = binding.description.text.toString()
+                if (coffeeapp.title.isNotEmpty()) {
+                    app.coffees.add(coffeeapp.copy())
+                    i("add Button Pressed: ${coffeeapp}")
+                    for (i in app.coffees.indices)
+                        { i("Coffeeapp[$i]:${this.app.coffees[i]}") }
+                }
+                else {
+                    Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                        .show()
+                }
             }
-            else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
-                    .show()
-            }
-        }
     }
 }
--- app/src/main/java/ie/setu/coffeeapp/main/MainApp.kt
+++ app/src/main/java/ie/setu/coffeeapp/main/MainApp.kt
@@ -1,11 +1,13 @@
 package ie.setu.coffeeapp.main
 
 import android.app.Application
+import ie.setu.coffeeapp.models.CoffeeAppModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
+    val coffees = ArrayList<CoffeeAppModel>()
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
