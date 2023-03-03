670a784ee215d9a0a91373643a151a150f97e1a9
Jefferaon <20103827@mail.wit.ie>
2023-03-03 14:52:27 +0000

    Added two new methods in CoffeeAppListActivity

--- app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppListActivity.kt
+++ app/src/main/java/ie/setu/coffeeapp/activities/CoffeeAppListActivity.kt
@@ -1,10 +1,16 @@
 package ie.setu.coffeeapp.activities
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ie.setu.coffeeapp.R
 import ie.setu.coffeeapp.databinding.ActivityCoffeeAppListBinding
 import ie.setu.coffeeapp.databinding.CardCoffeeappBinding
 import ie.setu.coffeeapp.main.MainApp
@@ -20,12 +26,41 @@ class CoffeeAppListActivity : AppCompatActivity() {
         binding = ActivityCoffeeAppListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
+
         app = application as MainApp
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = CoffeeAppAdapter(app.coffees)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> {
+                val launcherIntent = Intent(this, CoffeeAppActivity::class.java)
+                getResult.launch(launcherIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private val getResult =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                (binding.recyclerView.adapter)?.
+                notifyItemRangeChanged(0,app.coffees.size)
+            }
+        }
+
 }
 
 class CoffeeAppAdapter constructor(private var coffees: List<CoffeeAppModel>) :
