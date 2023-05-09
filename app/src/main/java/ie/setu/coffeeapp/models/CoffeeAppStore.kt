4643d25bdeb26c983ba482d6ed5a19de0385d671
Jefferaon <20103827@mail.wit.ie>
2023-05-09 13:07:00 +0100

    Added delete code in CoffeeAppStore

--- app/src/main/java/ie/setu/coffeeapp/models/CoffeeAppStore.kt
+++ app/src/main/java/ie/setu/coffeeapp/models/CoffeeAppStore.kt
@@ -4,4 +4,6 @@ interface CoffeeAppStore {
     fun findAll(): List<CoffeeAppModel>
     fun create(coffeeapp: CoffeeAppModel)
     fun update(coffeeapp: CoffeeAppModel)
+    fun delete(coffeeapp: CoffeeAppModel)
+
 }
